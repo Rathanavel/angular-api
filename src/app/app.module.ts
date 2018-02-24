2193bd73b3e0e3c8de52b95e4d7b87a7d9148150
Rathanavel <Rathanavel@github.com>
2018-02-24 12:44:23 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { QuickLinksComponent } from './quick-links/quick-links.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuickLinksComponent,
+    NavBarComponent,
+    CarouselComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
